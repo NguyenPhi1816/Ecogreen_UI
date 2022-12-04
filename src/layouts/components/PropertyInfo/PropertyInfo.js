7ea0d5252b8f173f9ef9a1ce3c154f4aeacb0403
Phi Nguyen <nguyenphi1816@gmail.com>
2022-12-04 14:59:28 +0700

    data changing #17

--- src/layouts/components/PropertyInfo/PropertyInfo.js
+++ src/layouts/components/PropertyInfo/PropertyInfo.js
@@ -91,35 +91,30 @@ function PropertyInfo({ offsetWidth, offsetY, currentItem, data }) {
                         </div>
                         <div className={cx('content')}>
                             <p>
-                                Lorem ipsum dolor sit amet, consectetuer
-                                adipiscing elit, sed diam nonummy nibh euismod
-                                tincidunt ut laoreet dolore magna aliquam erat
-                                volutpat. Ut wisi enim ad minim veniam, quis
-                                nostrud exerci tation ullamcorper suscipit
-                                lobortis nisl ut aliquip ex ea commodo
-                                consequat. Duis autem vel eum iriure dolor in
-                                hendrerit in vulputate velit esse molestie
-                                consequat, vel illum dolore eu feugiat nulla
-                                facilisis at vero eros et accumsan et iusto odio
-                                dignissim qui blandit praesent luptatum zzril
-                                delenit augue duis dolore te feugait nulla
-                                facilisi.
+                                “Eco Green Saigon” is located at the southern
+                                gateway of Ho Chi Minh City. It is the
+                                intersection of 3 largest urban areas: Center of
+                                Ho Chi Minh City - Phu My Hung International
+                                Financial Center - New administrative center of
+                                Thu Thiem city.
                             </p>
                             <p>
-                                Nam liber tempor cum soluta nobis eleifend
-                                option congue nihil imperdiet doming id quod
-                                mazim placerat facer possim assum. Typi non
-                                habent claritatem insitam; est usus legentis in
-                                iis qui facit eorum claritatem. Investigationes
-                                demonstraverunt lectores legere me lius quod ii
-                                legunt saepius. Claritas est etiam processus
-                                dynamicus, qui sequitur mutationem consuetudium
-                                lectorum. Mirum est notare quam littera gothica,
-                                quam nunc putamus parum claram, anteposuerit
-                                litterarum formas humanitatis per seacula quarta
-                                decima et quinta decima. Eodem modo typi, qui
-                                nunc nobis videntur parum clari, fiant sollemnes
-                                in futurum.
+                                Eco Green Saigon is a complex of commerce,
+                                services, hotels and apartments with a system of
+                                high-class internal utilities.
+                            </p>
+                            <p>
+                                Bustling commercial center and Grand Hyatt hotel
+                                with 69 floors (600 rooms) and 30,000 square
+                                meters. Along with a large-scale wedding
+                                conference center of 2,300m2 and an automatic
+                                generator system to supply electricity to all
+                                apartments when a local power failure occurs.
+                                Eco Green apartment project District 7 is a
+                                5-star complex with a huge scale of 14.36
+                                hectares including 7 apartment blocks, 1 office
+                                building - Park Hyatt luxury hotel - the tallest
+                                tower in District 7 with height 68 Floors.
                             </p>
                         </div>
                     </div>

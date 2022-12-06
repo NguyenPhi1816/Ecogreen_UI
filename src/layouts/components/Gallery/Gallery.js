af9c28d79dd8ccd21f2d85d801f7d7c9469c8c6a
Phi Nguyen <nguyenphi1816@gmail.com>
2022-12-06 10:35:52 +0700

    data changing #27

--- src/layouts/components/Gallery/Gallery.js
+++ src/layouts/components/Gallery/Gallery.js
@@ -8,21 +8,13 @@ const cx = classNames.bind(styles);
 
 function Gallery({ id }) {
     const [data, setData] = useState([]);
-    const [currentImages, setCurrentImages] = useState([]);
-    const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
         get(child(dbRef, `gallery`))
             .then((snapshot) => {
                 if (snapshot.exists()) {
-                    setData(snapshot.val());
-                    setCurrentImages(() => {
-                        let images = [];
-                        for (let i = 0; i < 7; i++) {
-                            images.push(snapshot.val()[i]);
-                        }
-                        return images;
-                    });
+                    let images = Object.values(snapshot.val());
+                    setData(images);
                 } else {
                     console.log('No data available');
                 }
@@ -32,30 +24,9 @@ function Gallery({ id }) {
             });
     }, []);
 
-    useEffect(() => {
-        const timerID = setInterval(() => {
-            setCurrentImages((prev) => {
-                let images = prev;
-                let image = data[Math.floor(Math.random() * 33)];
-                while (prev.includes(image)) {
-                    image = data[Math.floor(Math.random() * 33)];
-                }
-                images[currentIndex] = image;
-                return images;
-            });
-            setCurrentIndex((prev) => {
-                return prev + 1 < 7 ? prev + 1 : 0;
-            });
-        }, 2000);
-
-        return () => {
-            clearInterval(timerID);
-        };
-    }, [currentIndex, data]);
-
     return (
         <section className={cx('gallery')} id={id}>
-            {data && currentImages && (
+            {
                 <div className={cx('gallery-container')}>
                     <div className={cx('gallery-title')}>
                         <h2 className={cx('gallery-title-main')}>
@@ -67,8 +38,8 @@ function Gallery({ id }) {
                         </p>
                     </div>
                     <div className={cx('gallery-grid')}>
-                        {currentImages &&
-                            currentImages.map((item, index) => (
+                        {data &&
+                            data.map((item, index) => (
                                 <div
                                     className={cx(
                                         'gallery-grid-item',
@@ -97,20 +68,11 @@ function Gallery({ id }) {
                                             {item.title}
                                         </h3>
                                     </div>
-                                    <div
-                                        className={cx('overlay', {
-                                            animate:
-                                                index ===
-                                                (currentIndex - 1 < 0
-                                                    ? 6
-                                                    : currentIndex - 1),
-                                        })}
-                                    ></div>
                                 </div>
                             ))}
                     </div>
                 </div>
-            )}
+            }
         </section>
     );
 }

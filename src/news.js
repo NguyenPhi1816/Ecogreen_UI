61668ca6cd660aca3995f98855a85b5e8d2f3c75
NguyenPhi1816 <nguyenphi1816@gmail.com>
2023-06-20 00:48:17 +0700

    update200623

--- src/news.js
+++ src/news.js
@@ -108,4 +108,154 @@ export const NEWS = [
             </>
         ),
     },
+    {
+        id: 'quan-7-co-gi-vui-cung-kham-pha-cuoc-song-quan-7-thanh-pho-ho-chi-minh',
+        title: 'Quận 7 có gì vui? Cùng khám phá cuộc sống quận 7 thành phố Hồ Chí Minh',
+        thumb: 'https://ecogreenq7.vn/rentalservicesg/news/quan-7-co-gi-vui-cung-kham-pha-cuoc-song-quan-7-thanh-pho-ho-chi-minh/crescent-mall-toa-lac-tai-quan-7-trung-tam-mua-sam-vui-choi-giai-tri-thu-hut-nhieu-nguoi-dan.jpg',
+        openingParagraph:
+            'Quận 7 từng là một phần của huyện Nhà Bè, sau đó được tách ra vào năm 1997,...',
+        body: (
+            <>
+                <p>
+                    Quận 7 từng là một phần của huyện Nhà Bè, sau đó được tách
+                    ra vào năm 1997, nơi đây có vị trí chiến lược về giao thông
+                    đường bộ và đường sông. Đồng thời là cửa ngõ phía Nam của
+                    Thành phố Hồ Chí Minh, cầu nối mở hướng phát triển của thành
+                    phố ra biển Đông cũng như ra thế giới.
+                </p>
+                <h2>Các địa điểm vui chơi giải trí tại quận 7</h2>
+                <p>
+                    Dù không có các khu du lịch, resort lớn nhưng quận 7 vẫn thu
+                    hút rất nhiều du khách bởi đây là thiên đường vui chơi, giải
+                    trí hấp dẫn dành cho mọi lứa tuổi. Vậy quận 7 có gì?
+                </p>
+                <h2>1. Bảo tàng tranh 3D Artinus</h2>
+                <p>
+                    Khi đặt chân vào nơi đây, ắt hẳn cảm giác đầu tiên ập tới
+                    bạn là bản thân thật nhỏ bé trong không gian sống ảo tràn
+                    ngập hình ảnh tranh 3D. Với đa đạng các chủ đề khác nhau,
+                    bạn sẽ cảm thấy như mình đang lạc vào trong một thế giới
+                    hoàn toàn mới lạ.
+                </p>
+                <ImgDescription
+                    src="https://ecogreenq7.vn/rentalservicesg/news/quan-7-co-gi-vui-cung-kham-pha-cuoc-song-quan-7-thanh-pho-ho-chi-minh/kham-pha-bao-tang-tranh-3D-Artinus.jpg"
+                    des="Khám phá bảo tàng tranh 3D Artinus"
+                />
+                <p>
+                    Bảo tàng tranh 3D Artinus được chia ra thành nhiều khu, như
+                    khu thế giới động vật hoang dã cho những ai thích khám phá
+                    thiên nhiên, khu nghệ thuật cổ đại và Ai Cập, khu đại dương.
+                    Bạn sẽ tích lũy thêm nhiều kiến thức về các giá trị văn hóa
+                    cổ xưa thông qua chuyến tham quan khám phá bảo tàng hội họa
+                    đầy ấn tượng này.
+                </p>
+                <p>
+                    Thú vị nhất là mỗi bức ảnh đều đi kèm hướng dẫn chi tiết và
+                    hình ảnh minh họa cụ thể giúp bạn tạo dáng đẹp nhất với góc
+                    chụp tuyệt vời. Đây là địa điểm vui chơi cực ổn cho các tín
+                    đồ sống ảo ngay trung tâm quận 7.
+                </p>
+                <h2>
+                    2. Crescent Mall – khu mua sắm tích hợp giải trí tại quận 7
+                </h2>
+                <p>
+                    Nếu bạn thích mua sắm, Crescent Mall chỉ cách Cầu Ánh Sao và
+                    Hồ Bán Nguyệt một đoạn ngắn, được chia thành các khu mua
+                    sắm, ăn uống và giải trí, với hơn 140 thương hiệu cao cấp.
+                    Du khách đến nơi đây đều bị mê hoặc bởi không gian tráng lệ
+                    và khung cảnh tráng lệ, đặc biệt là trong các dịp lễ quan
+                    trọng như Halloween, Giáng sinh và Năm mới.
+                </p>
+                <ImgDescription
+                    src="https://ecogreenq7.vn/rentalservicesg/news/quan-7-co-gi-vui-cung-kham-pha-cuoc-song-quan-7-thanh-pho-ho-chi-minh/crescent-mall-toa-lac-tai-quan-7-trung-tam-mua-sam-vui-choi-giai-tri-thu-hut-nhieu-nguoi-dan.jpg"
+                    des="Crescent Mall toạ lạc tại quận 7 – trung tâm mua sắm, vui chơi giải trí thu hút nhiều người dân"
+                />
+                <p>
+                    Khi đặt chân đến nơi đây, bạn sẽ thoải thích khám phá các
+                    khu vui chơi khác nhau. Hơn nữa, nơi đây còn gây ấn tượng
+                    khi sở hữu nhiều góc chụp ảnh lạ mắt, tạo sự thích thú và
+                    được nhiều bạn trẻ đặc biệt yêu thích. Nếu bạn là một thành
+                    viên của cộng đồng sống ảo thì còn chần chừ gì nữa mà không
+                    nhanh xách ba lô lên và đến ngay khu vui chơi này để khám
+                    phá. Một điểm đặc sắc nữa là nơi đây vào ban đêm luôn ngập
+                    tràn màu sắc của ánh đèn chiếu sáng, tạo nên một hiệu ứng
+                    màu sắc không thể sống động hơn.
+                </p>
+                <h2>3. Cầu Phú Mỹ lung linh</h2>
+                <p>
+                    Nối liền quận 2 với quận 7, cầu Phú Mỹ là cây cầu dây được
+                    nhận định là lớn nhất tại Sài Gòn bởi chiều dài lên tới 2km.
+                    Điểm thu hút nổi bật của chiếc cầu này là sự ấn tượng trong
+                    phong cách thiết kế đương đại. Đứng từ điểm quan sát trên
+                    cầu cầu Phú Mỹ nhìn xuống, dường như bạn sẽ được chiêm
+                    ngưỡng gần như là toàn bộ những cảnh sắc tuyệt đẹp, lung
+                    linh ở quận 7. Vì thế, khi thắc mắc quận 7 có gì vui thì các
+                    bạn nên cân nhắc đến tham quan địa điểm này.
+                </p>
+                <ImgDescription
+                    src="https://ecogreenq7.vn/rentalservicesg/news/quan-7-co-gi-vui-cung-kham-pha-cuoc-song-quan-7-thanh-pho-ho-chi-minh/cau-phu-my-noi-lien-quan-2-va-quan-7-voi-canh-hoang-hon-tuyet-dep.jpg"
+                    des="Cầu Phú Mỹ nối liền quận 2 và quận 7 với cảnh hoàng hôn tuyệt đẹp"
+                />
+                <p>
+                    Theo góc nhìn từ cầu Phú Mỹ nhìn ra, bạn sẽ được chiêm
+                    ngưỡng cảnh sắc hiện lên đa sắc màu với biết bao nhiêu là
+                    ánh đèn điện huyền ảo được giăng kín lối. Đặc biệt phải nhắc
+                    đến là cảnh hoàng hôn đắt giá khi đứng ngắm từ vị trí cầu
+                    Phú Mỹ là vô cùng nên thơ và đẹp mắt đến nao lòng. Khi hướng
+                    mắt về quận 2, bạn sẽ cảm nhận được vẻ đẹp của quận 2 hiện
+                    lên như hình ảnh con rồng đang uốn lượn trên bầu trời xanh
+                    và một quận 7 tràn ngập sắc màu tươi đẹp.
+                </p>
+                <h2>Quận 7 có gì ngoài khu mua sắm, giải trí?</h2>
+                <p>
+                    Bên cạnh những địa điểm vui chơi giải trí, mua sắm nêu trên,
+                    quận 7 còn sở hữu một khu phức hợp đặc sắc với phong cách
+                    kiến trúc hiện đại và tiện ích cao cấp.
+                </p>
+                <h2>Chung cư Eco Green Sài Gòn </h2>
+                <p>
+                    Eco Green Sài Gòn toạ lạc tại quận 7 được đánh giá là một
+                    trong những khu đô thị cao cấp với hệ thống tiện ích nội khu
+                    bậc nhất. Với sự kết hợp giữa thương mại, dịch vụ, khách
+                    sạn, và nhà ở sang trọng, cùng với các trang thiết bị nội
+                    thất đẳng cấp, nơi đây tạo ra môi trường sống an lành cho cư
+                    dân.
+                </p>
+                <ImgDescription
+                    src="https://ecogreenq7.vn/rentalservicesg/news/quan-7-co-gi-vui-cung-kham-pha-cuoc-song-quan-7-thanh-pho-ho-chi-minh/eco-green-sai-gon-voi-phong-cach-kien-truc-hien-dai-tien-ich-cao-cap.jpg"
+                    des="Eco Green Sài Gòn với phong cách kiến trúc hiện đại, tiện ích cao cấp"
+                />
+                <p>
+                    Hơn nữa, người dân sở hữu căn hộ nơi đây không những lợi thế
+                    về vị trí đắc địa mà còn được hưởng nhiều lợi ích từ cơ sở
+                    hạ tầng của khu vực. Vì vậy, căn hộ Eco Green Sài Gòn đang
+                    thu hút sự quan tâm của rất nhiều khách hàng. Tại đây, khách
+                    hàng khi mua có thể làm nơi an cư lâu dài hoặc bán lại cũng
+                    thu về giá cao.
+                </p>
+                <h2>Lời kết:</h2>
+                <p>
+                    Quận 7 được xem là một trong những quân trung tâm, không
+                    những sở hữu nhiều dịch vụ, khu trung tâm mua săm, giải trí,
+                    mà còn là nơi an cư lý tưởng. Nếu bạn đang tìm kiếm một
+                    không gian sống trong lành, tiện nghi đầy đủ và thuận tiện
+                    cho việc di chuyển. Hãy đến với Eco Green Sài Gòn để trải
+                    nghiệm cuộc sống xứng đáng với giá trị của bạn.
+                </p>
+                <p>
+                    Eco Green Sài Gòn còn được đánh giá là một trong những dự án
+                    căn hộ cao cấp nhưng giá rẻ, đặc biệt có vị trí “vàng” gần
+                    quận 1 và quận 4. Với mức giá hợp lý nhưng vẫn đảm bảo chất
+                    lượng, đây là cơ hội tuyệt vời cho những ai muốn sở hữu một
+                    căn hộ tại khu vực trung tâm của thành phố hoặc đang tìm
+                    kiếm một cơ hội đầu tư tiềm năng trong lĩnh vực bất động
+                    sản.
+                </p>
+                <p>
+                    <a href="#footer">Liên hệ ngay</a> để được tư vấn và tham
+                    quan căn hộ mẫu của dự án.
+                </p>
+            </>
+        ),
+    },
 ];
